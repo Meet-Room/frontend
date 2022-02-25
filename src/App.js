13c5b80b41b46a8d46194f439c27fd27e9254d6e
Mrinmay Bhattacharyya <mrinmay678@pop-os.localdomain>
2022-02-25 10:18:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Box } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Box 
+      bg="brand.100"
+      m={["32px","100px","25%"]}
+      fontSize={["32px","40px","50px"]}
+    >
+      Welcome
+    </Box>
+  );
+}
+
+export default App;
